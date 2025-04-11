27413e6d15af1d3233794332203eb185bede63fe
dtlp97 <drew.perry@mrm.com>
2025-04-11 16:01:34 -0400

    added delete button functionality in JS

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ taskForm.addEventListener('submit' , function(e){
     var newTaskInputValue = newTaskInput.value;
     // console.log('newTaskInputValue: ', newTaskInputValue);
 
-    addTaskItem(newTaskInputValue);
+    //addTaskItem(newTaskInputValue);
+    addTaskNew(newTaskInputValue);
 
     //reset input value to empty
     newTaskInput.value = '';
@@ -90,7 +91,43 @@ function addTaskItem(newTask){
      newTaskItem.appendChild(deleteButton);
 
      taskList.appendChild(newTaskItem);
-     
+} 
+// To add task in List
+
+     function addTaskNew(newTask) {
+    const newTaskItem = `
+    <li class="taskItem">
+          <label class="checkContainer">
+            <input class="checkbox" type="checkbox" />
+            <span class="checkmark"></span>
+          </label>`+newTask+`
+          <div class="deleteButton">
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" height="24" 
+                viewBox="0 0 24 24" 
+                fill="none" stroke="currentColor" 
+                stroke-width="2" stroke-linecap="round" 
+                stroke-linejoin="round" 
+                class="lucide lucide-trash-2">
+                <path d="M3 6h18"/>
+                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
+                <line x1="10" x2="10" y1="11" y2="17"/>
+                <line x1="14" x2="14" y1="11" y2="17"/>
+            </svg>
+          </div>
+        </li>
+    `;
+    $(taskList).append(newTaskItem); 
 }
+
+taskList.addEventListener('click',function(e){
+    if(e.target.closest('.deleteButton')){
+        // console.log('delete this')
+        const taskItem= e.target.closest('.taskItem')
+        taskItem.remove();
+    } 
+}) 
 
 
